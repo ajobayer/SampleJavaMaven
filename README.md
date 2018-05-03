20ad413d2666ac04d2a39b83335ea26c0a007e0a
Abdullah Jobayer <ajobayer@gmail.com>
2018-05-03 16:53:56 +1000

    Initial commit to setup sample Java Maven Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# README #
+
+Jira(issues tracking) -> Bitbucket(source code tracking) -> Bitbucket Pipeline(build & unit test) -> Crucible(review)
---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello world! this is for pull request second time";
+	}
+}
